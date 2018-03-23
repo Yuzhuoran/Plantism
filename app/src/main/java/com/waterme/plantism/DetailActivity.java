5e1c3f4e647654315012e8d1006127f322bb2fc3
zhuoran <pkuyzr@gmail.com>
2018-03-22 21:23:29 -0400

    update detail activity

--- app/src/main/java/com/waterme/plantism/DetailActivity.java
+++ app/src/main/java/com/waterme/plantism/DetailActivity.java
@@ -1,23 +1,80 @@
 package com.waterme.plantism;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TableLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private static String TAG = "Detail Activity";
+
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private String uid;
+    private String plantId;
+    private String plantCategory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        // get the current username
+        SharedPreferences sharedPre = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        uid = sharedPre.getString("uid","");
+
+
+
+
+
+        // set up page
+        mViewPager = (ViewPager) findViewById(R.id.vp_detail);
+        setUpViewPager(mViewPager);
+        mTabLayout = (TabLayout) findViewById(R.id.tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+
+
     }
 
+    private void setUpViewPager(ViewPager viewPager) {
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            plantId = bundle.getString("PLANTID");
+            plantCategory = bundle.getString("CATEGORY");
+        } else {
+            Log.d(TAG, "new plant id pass in!");
+        }
+        bundle.putString("UID", uid);
+        bundle.putString("PLANTID", plantId);
+        bundle.putString("CATEGORY", plantCategory);
+        //
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+
+        StatusFragment statusFragment = new StatusFragment();
+        statusFragment.setArguments(bundle);
+
+        GuideFragment guideFragment = new GuideFragment();
+        guideFragment.setArguments(bundle);
+
+        adapter.addFragment(statusFragment, "STATUS");
+        adapter.addFragment(guideFragment, "GUIDE");
+
+        viewPager.setAdapter(adapter);
+
+    }
     public class Adapter extends FragmentPagerAdapter {
         List<Fragment> mFragmentList = new ArrayList<>();
         List<String> mFragmentTitleList = new ArrayList<>();
--- app/src/main/java/com/waterme/plantism/StatusFragment.java
+++ app/src/main/java/com/waterme/plantism/StatusFragment.java
@@ -1,11 +1,36 @@
 package com.waterme.plantism;
 
-import android.app.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
 
 /**
  * Created by zhuoran on 3/22/18.
  */
 
 public class StatusFragment extends Fragment {
+    private static String PLANTID = "PLANTID";
+    private static String UID = "UID";
+    private String plantid;
+    private String uid;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            plantid = getArguments().getString(PLANTID);
+            uid = getArguments().getString(UID);
+        }
+    }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.content_status, container, false);
+
+        return rootView;
+    }
 }
